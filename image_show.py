60b05a6881279620c1565b11c6a93b44b406a3ef
Francesco James Fanti <97003970+neoSnakex34@users.noreply.github.com>
2022-02-17 02:43:22 +0100

    Add files via upload

---
+++ image_show.py
@@ -0,0 +1,16 @@
+from PIL import Image 
+
+def show(path):
+    
+    img = Image.open(path)
+    width, height = img.size
+
+    resized_image = img.resize(
+            (round(img.size[0]*0.3),round(img.size[1]*0.3))
+            )
+
+    resized_image.show()
+
+if __name__ == '__main__':
+    path = '/home/james/Immagini/rei.png'
+    show(path)
